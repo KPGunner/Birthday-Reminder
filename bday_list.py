7a25b8b2c931ca046e2c1f0adf43f5aa9790dfcc
Gunn <31373828+KPGunner@users.noreply.github.com>
2018-11-28 11:12:29 -0500

    Update bday_list.py

--- bday_list.py
+++ bday_list.py
@@ -2,19 +2,33 @@ import time
 
 today = time.strftime('%m%d')
 print(today)
+week = int(today) + 7
+week = str(week)
+week = week.zfill(4)
+#print(week)
 
 birthdays = {'This Script': '0816', 'Individuals Name': 'DOB1'}
 
 
-def birthday_wish():
+def birthday_advance():
     for k, v in birthdays.items():
         # print(k ,v)
-        if today in v:
-            return('\nWish ' + k + ' a happy birthday!' )
-            break
+        if week in v:
+            return'\nWish ' + k + ' a happy birthday on ' + v + '!'
     else:
         pass
 
+    
+def birthday_wish():
+    for k, v in birthdays.items():
+        if today in v:
+            return '\nWish ' + k + ' a happy birthday today!'
+    else:
+        pass
+
+
 birthday_wish()
+birthday_advance()
 
 print(birthday_wish())
+print(birthday_advance())
